4ade5828d2e5cc8feb3bdbd8a6ae4cd7f078bfcf
fidashn <87206839+fidashn@users.noreply.github.com>
2022-09-15 20:22:25 +0300

    created basic btns style

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -3,7 +3,10 @@ import React from 'react'
 function Home() {
   return (
     <div className='home-container'>
-      <div className="counter"><button></button></div>
+      <div className="counter">
+        <button className='btn'>-</button>
+        <button className='btn'>+</button>
+        </div>
       
       </div>
   )
