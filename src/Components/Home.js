1c9bc4d0fb1445867ace52c9f82bf30afac83d17
fidashn <87206839+fidashn@users.noreply.github.com>
2022-09-17 19:25:35 +0300

    added title

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -3,9 +3,10 @@ import React from 'react'
 function Home() {
   return (
     <div className='home-container'>
+      <h1>Counter</h1>
       <div className="counter">
         <button className='btn red'>-</button>
-        <div className='number'>1</div>
+        <div className='number'>0</div>
         <button className='btn green'>+</button>
         </div>
       
