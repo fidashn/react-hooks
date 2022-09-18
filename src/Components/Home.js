5dae884c89e14886893fb496041731fd47cf2d86
fidashn <87206839+fidashn@users.noreply.github.com>
2022-09-18 15:53:48 +0300

    added react hook

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,13 +1,16 @@
-import React from 'react'
+import React, {useState }  from 'react';
+
+
 
 function Home() {
+const [count, setCount] = useState(0);
   return (
     <div className='home-container'>
       <h1>Counter</h1>
       <div className="counter">
-        <button className='btn red'>-</button>
+        <button className='btn red' onClick={() => setCount(count - 1)}>-</button>
         <div className='number'>0</div>
-        <button className='btn green'>+</button>
+        <button className='btn green' onClick={() => setCount(count + 1)}>+</button>
         </div>
       
       </div>
